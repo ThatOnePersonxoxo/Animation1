7f6d102f638c5a64349ac052ed455b075d6e67cf
sakthisubhamohan <136231636+sakthisubhamohan@users.noreply.github.com>
2023-06-11 15:26:52 +0530

    Update animation1.py

--- animation1.py
+++ animation1.py
@@ -14,18 +14,18 @@ a2.speed(15)
 a2.pencolor('#006400')
 for i in range (350):
     a2.forward(i)
-    a2.left(50)
+    a2.right(91)
 
 a3 = turtle.Turtle()
 a3.speed(20)
 a3.pencolor('#FFD700')
-for i in range (300):
+for i in range (400):
     a3.forward(i)
-    a3.left(20)
+    a3.right(91)
 
-a2 = turtle.Turtle()
-a2.speed(15)
-a2.pencolor('#CAFF70')
-for i in range (350):
-    a2.forward(i)
-    a2.left(100)
+a4 = turtle.Turtle()
+a4.speed(15)
+a4.pencolor('#CAFF70')
+for i in range (400):
+    a4.forward(i)
+    a4.left(300)
